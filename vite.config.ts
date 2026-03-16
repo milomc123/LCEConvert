7c01ef162f7c2f5602e3ffad2e492fec4953d079
milomc123 <33701036+milomc123@users.noreply.github.com>
2026-03-16 14:11:32 +0000

    Setup Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 
+const inGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+const repository = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const isUserOrOrgPagesRepo = repository.endsWith('.github.io');
+const ghPagesBase = repository && !isUserOrOrgPagesRepo ? `/${repository}/` : '/';
+
 export default defineConfig({
+  base: inGitHubActions ? ghPagesBase : '/',
   server: {
     port: 5173,
   },
